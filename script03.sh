298712352ad114536e8b8cfb77e685712963bbc3
m.saigopi <msaigopi91@gmail.com>
2024-10-05 23:03:55 +0530

    Update script03.sh

--- script03.sh
+++ script03.sh
@@ -4,11 +4,11 @@
 # Date Created:
 # Version: 
 # Date Modified:
-<<PRASHANTH
+<<SAIGOPI
 this is a simple for loop to print Hello world 10 times
 This program once executed the loop continues for 10 times
 For is a loop which accepts variable definition, condition and increment
-PRASHANTH
+SAIGOPI
 for((i=0;i<11;i++))
 do
 	echo "$i Hello world"
